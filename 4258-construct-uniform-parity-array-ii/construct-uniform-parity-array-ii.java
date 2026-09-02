e8a85b004e6bc28044abecfe25ccdae636268022
Onkar Bhojane <122461938+onkarbhojane@users.noreply.github.com>
2026-09-02 23:49:08 +0530

    Time: 6 ms (56.12%) | Memory: 118.6 MB (95.68%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean uniformArray(int[] nums1) {
         int cnt=0;
         int[] arr=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++) arr[i]=nums1[i];
-        Arrays.sort(arr);
-        int par=arr[0]%2;
+        int min=nums1[0];
+        for(int i=0;i<nums1.length;i++) min=Math.min(nums1[i],min);
+        int par=min%2;
         for(int i=0;i<nums1.length;i++){
             if(nums1[i]%2==0) cnt++;
         }
